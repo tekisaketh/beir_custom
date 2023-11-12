875443b9389924be92583a34272331b8ea96299f
tekisaketh <venkatsakethteki@gmail.com>
2023-11-12 22:03:51 +0100

    query_embeddings

--- beir/retrieval/search/dense/faiss_search.py
+++ beir/retrieval/search/dense/faiss_search.py
@@ -13,11 +13,12 @@ logger = logging.getLogger(__name__)
 #Parent class for any faiss search
 class DenseRetrievalFaissSearch(BaseSearch):
     
-    def __init__(self, model, corpus_embeddings: np.ndarray=None,faiss_ids: np.ndarray=None, batch_size: int = 128, corpus_chunk_size: int = 50000, use_gpu: bool = False, **kwargs):
+    def __init__(self, model, corpus_embeddings: np.ndarray=None,faiss_ids: np.ndarray=None, query_embeddings: np.ndarray=None,batch_size: int = 128, corpus_chunk_size: int = 50000, use_gpu: bool = False, **kwargs):
         print("okay")
         self.model = model
         self.batch_size = batch_size
         self.corpus_embeddings = corpus_embeddings
+        self.query_embeddings = query_embeddings
         self.faiss_ids = faiss_ids
         self.corpus_chunk_size = corpus_chunk_size
         self.score_functions = ['cos_sim','dot']
@@ -107,6 +108,19 @@ class DenseRetrievalFaissSearch(BaseSearch):
 
             return faiss_ids, corpus_embeddings
     
+    def create_embeddings(self, queries: Dict[str, str],score_function = str):
+        
+        normalize_embeddings = True if score_function == "cos_sim" else False
+        query_ids = list(queries.keys())
+        queries = [queries[qid] for qid in queries]
+        print("Computing Query Embeddings. Normalize: {}...".format(normalize_embeddings))
+        query_embeddings = self.model.encode_queries(
+            queries, show_progress_bar=True, 
+            batch_size=self.batch_size, 
+            normalize_embeddings=normalize_embeddings)
+        
+        return query_embeddings
+
     def search(self, 
                corpus: Dict[str, Dict[str, str]],
                queries: Dict[str, str], 
@@ -118,13 +132,8 @@ class DenseRetrievalFaissSearch(BaseSearch):
 
         if not self.faiss_index: self.index(corpus, score_function)
 
-        query_ids = list(queries.keys())
-        queries = [queries[qid] for qid in queries]
-        print("Computing Query Embeddings. Normalize: {}...".format(normalize_embeddings))
-        query_embeddings = self.model.encode_queries(
-            queries, show_progress_bar=True, 
-            batch_size=self.batch_size, 
-            normalize_embeddings=normalize_embeddings)
+        if(self.query_embeddings==None):
+            query_embeddings = self.create_embeddings(queries=queries, score_function=score_function)
         
         faiss_scores, faiss_doc_ids = self.faiss_index.search(query_embeddings, top_k, **kwargs)
         
